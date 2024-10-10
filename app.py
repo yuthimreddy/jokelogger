3e9b10d147b594a400aa8a51d06b9f709a4a55b9
Yuthi Madireddy <hva4zb@virginia.edu>
2024-10-10 14:38:47 +0000

    cleaning up

--- app.py
+++ app.py
@@ -14,5 +14,10 @@ id = response['id']
 setup = response['setup']
 punchline = response['punchline']
 
-print(id, setup, punchline)
+#print(id, setup, punchline)
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename="joke.log", encoding="utf-8", level=logging.DEBUG)
+
+logging.warning('%s:%s:%s', id, setup, punchline)
 
